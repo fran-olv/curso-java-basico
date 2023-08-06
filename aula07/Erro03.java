b54a525aada7155f7735287fe1d03f7bd1b3a7b5
Franciele B. de Oliveira <32960110+fran-olv@users.noreply.github.com>
2023-08-06 02:04:35 -0300

    Update Erro03.java

--- aula07/Erro03.java
+++ aula07/Erro03.java
@@ -1,7 +1,5 @@
 class Erro03 {
-	
-	public static void main (String[] args){
-
+	public static void Main (String[] args){
 		//System.out.println("Voce digitou: ");
 		System.out.println("Voce digitou: " + 1/0);
 	}
